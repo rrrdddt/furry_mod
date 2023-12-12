6c9096991f77b0d6db2009e38d389a57f3dd3932
rrrdddt <121048677+rrrdddt@users.noreply.github.com>
2023-12-12 21:18:08 +0200

    Update mod.js

--- mod.js
+++ mod.js
@@ -1,4 +1,6 @@
 // Defines
+const SYMBOLS = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!?\"\`\'#()[].,_- \n'.split('');
+const FAPI = window.fapi;
 const ModalHandler = FAPI.imodules.ModalHandler;
 const ChunkUpdates = FAPI.routes.ChunkUpdates;
 
