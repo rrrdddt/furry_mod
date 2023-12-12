9b0082736ab045168a9c3a2e04edd4f5648e1def
rrrdddt <121048677+rrrdddt@users.noreply.github.com>
2023-12-12 21:51:34 +0200

    Update mod.js

--- mod.js
+++ mod.js
@@ -54,74 +54,7 @@ rgb_lamp.save_cd = (arrow) => {
     return [(arrow.custom_data[0] << 5) | (arrow.custom_data[1] << 3) | arrow.custom_data[2]];
 }
 rgb_lamp.custom_data = [4, 0, 0];
-// endregion
-// region PURPLE_ARROW
-const purple_arrow = mod.registerArrow(1)
-purple_arrow.name = ['Purple arrow','Фиолетовая стрелка','Фіолетова стрілка','Фіялетавая стрэлка'];
-purple_arrow.activation = ["On any incoming signal.","Любым входящим сигналом.","Будь-яким вхідним сигналом.","Любым уваходным сігналам."];
-purple_arrow.action = ["Sends a signal forwards, skipping `n` cells.","Передает сигнал вперед через `n` клеток.","Передає сигнал вперед через `n` клітини.","Перадае сігнал наперад праз `n` клеткі."];
-purple_arrow.icon_url = "https://raw.githubusercontent.com/Fotiska/X-DLC/main/images/purple_arrow.png";
-purple_arrow.textures = ["https://raw.githubusercontent.com/Fotiska/X-DLC/main/images/purple_arrow.png", "https://raw.githubusercontent.com/Fotiska/X-DLC/main/images/purple_diagonal_arrow.png"];
-purple_arrow.clickable = true;
-purple_arrow.update = (arrow) => {
-    if (arrow.signalsCount > 0) arrow.signal = 6;
-    else arrow.signal = 0;
-}
-purple_arrow.transmit = (arrow) => {
-    if (arrow.signal === 6) ChunkUpdates.updateCount(arrow, ChunkUpdates.getArrowAt(arrow.chunk, arrow.x, arrow.y, arrow.rotation, arrow.flipped, -arrow.custom_data[0], arrow.custom_data[1]));
-}
-purple_arrow.click = (arrow) => {
-    const modal = ModalHandler.showModal();
-    const xSelect = modal.createInput('Вперёд', 'От 0 до 15')
-    xSelect.value = arrow.custom_data[0];
-    xSelect.onchange = () => arrow.custom_data[0] = Math.max(0, Math.min(15, xSelect.value));
-
-    const ySelect = modal.createInput('Вбок', 'От 0 до 15')
-    ySelect.value = arrow.custom_data[1];
-    ySelect.onchange = () => arrow.custom_data[1] = Math.max(0, Math.min(15, ySelect.value));
-}
-purple_arrow.draw = (arrow, index) => {
-    if (arrow.custom_data !== undefined && arrow.custom_data[1] !== 0) return index + 1;
-    return index;
-}
-purple_arrow.load_cd = (cd) => {
-    let x = (cd[0] >> 4) & 0b1111;
-    let y = cd[0] & 0b1111;
-    return [x, y];
-}
-purple_arrow.save_cd = (arrow) => {
-    return [(arrow.custom_data[0] << 4) | arrow.custom_data[1]];
-}
-purple_arrow.custom_data = [2, 0];
-// endregion
-// region TEXT_BLOCK
-function text2seq(text) {
-    let seq = [];
-    text.split('').forEach((symbol) => {
-        let index = SYMBOLS.indexOf(symbol);
-        if (index !== -1) seq.push(index);
-    });
-    return seq;
-}
-function seq2text(seq) {
-    let text = '';
-    seq.forEach((val) => text += SYMBOLS[val]);
-    return text;
-}
-
-const text_block = mod.registerArrow(2)
-text_block.name =['Block of text','Блок текста','Блок текста','Блок текста'];
-text_block.activation = ["On any incoming signal.","Любым входящим сигналом.","Будь-яким вхідним сигналом.","Любым уваходным сігналам."];
-text_block.action =["Sends a signal diagonally, skipping `n` cells.","Передает сигнал по диагонали через `n` клеток.","Передає сигнал вперед через `n` клітини.","Перадае сігнал наперад праз `n` клеткі."];
-text_block.icon_url = "https://raw.githubusercontent.com/Fotiska/X-DLC/main/images/text_block.png";
-text_block.clickable = true;
-text_block.click = (arrow) => {
-    const modal = ModalHandler.showModal();
-    const textArea = modal.createTextArea('Текст', 'Введите текст')
-    textArea.value = seq2text(arrow.custom_data);
-    textArea.onchange = () => arrow.custom_data = text2seq(textArea.value);
-}
-// endregion
+// endregionegion
 // endregion
 
 // TODO: Рандомайзер с настраиваемым рандомом
